4a0aecb7b9d23e8526c244f51703ddf90ee47724
Jayasurya28 <jayasurya1970@gmail.com>
2025-02-18 17:44:50 +0530

    views and auth

---
+++ website/auth.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+auth = Blueprint("auth", __name__)
+
+@auth.route("/login")
+def login():
+    return "Login"
+
+@auth.route("/sign-up")
+def sign_out():
+    return "Sign Up"
+
+@auth.route("/logout")
+def logout():
+    return "Logout"
+
+ 
+
+
+ 
