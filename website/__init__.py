e3581a0998dd30b445d8957e6fe08036e4612399
Jayasurya28 <jayasurya1970@gmail.com>
2025-02-19 01:36:20 +0530

    bugsfixed

--- website/__init__.py
+++ website/__init__.py
@@ -2,24 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
 def create_app(): 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    csrf = CSRFProtect()
+    csrf.init_app(app)
+
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views,url_prefix="/")
-    app.register_blueprint(auth,url_prefix="/")
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     from .models import User
 
-
-    create_database(app)
+    create_database(app)  # Ensure database is created
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
@@ -29,10 +33,10 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
-    return app 
+    return app
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists("instance/" + DB_NAME):
         with app.app_context():
             db.create_all()
-            print('Created Database!')
+            print("Database created successfully!")
--- website/models.py
+++ website/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150) , unique=True)
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    date_created = db.Column(db.DateTime, default=func.now())
