a8dece38a4f95a4071222326acf47ccf38664c06
Jayasurya28 <jayasurya1970@gmail.com>
2025-02-19 14:10:05 +0530

    added delete dropdown

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template , request,flash
+from flask import Blueprint, render_template , request,flash,redirect,url_for
 from flask_login import login_required ,current_user
 from .models import Post
 from . import db 
@@ -8,7 +8,8 @@ views = Blueprint("views", __name__)
 @views.route("/")
 @views.route("/home")
 def home():
-    return render_template("home.html", user=current_user)
+    posts = Post.query.all()
+    return render_template("home.html", user=current_user,posts=posts)
 
 @views.route("/create-post",methods=['GET', 'POST'])
 @login_required
@@ -22,4 +23,5 @@ def create_post():
             db.session.add(post)
             db.session.commit()
             flash("Post created!", category="success")
+            return redirect(url_for("views.home"))
     return render_template("create_post.html", user=current_user)
