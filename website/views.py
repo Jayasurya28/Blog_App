b6cef6e00ddf69aa7416ff016161369ae0c515e1
Jayasurya28 <jayasurya1970@gmail.com>
2025-02-21 21:23:16 +0530

    completed V3

--- website/views.py
+++ website/views.py
@@ -1,30 +1,36 @@
-from flask import Blueprint, render_template , request,flash,redirect,url_for
-from flask_login import login_required ,current_user
-from .models import Post
-from . import db 
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
+from .models import Post, User
+from . import db
 
 views = Blueprint("views", __name__)
+
 
 @views.route("/")
 @views.route("/home")
 def home():
-    posts = Post.query.all()
-    return render_template("home.html", user=current_user,posts=posts)
+    if current_user.is_authenticated:
+        posts = Post.query.all()
+        return render_template("home.html", user=current_user, posts=posts)
+    return render_template("home.html", user=current_user)
 
-@views.route("/create-post",methods=['GET', 'POST'])
+
+@views.route("/create-post", methods=['GET', 'POST'])
 @login_required
 def create_post():
     if request.method == "POST":
-        text = request.form.get("text")
+        text = request.form.get('text')
+
         if not text:
-            flash("Post cannot be empty", category="error")
+            flash('Post cannot be empty', category='error')
         else:
             post = Post(text=text, author=current_user.id)
             db.session.add(post)
             db.session.commit()
-            flash("Post created!", category="success")
-            return redirect(url_for("views.home"))
-    return render_template("create_post.html", user=current_user)
+            flash('Post created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template('create_post.html', user=current_user)
 
 
 @views.route("/delete-post/<id>")
@@ -33,14 +39,25 @@ def delete_post(id):
     post = Post.query.filter_by(id=id).first()
 
     if not post:
-        flash('Post does not exist.', category='error')
-    elif current_user.id != post.id:
+        flash("Post does not exist.", category='error')
+    elif current_user.id != post.author:
         flash('You do not have permission to delete this post.', category='error')
     else:
         db.session.delete(post)
         db.session.commit()
-        flash('Post deleted!', category='success') 
+        flash('Post deleted.', category='success')
+
+    return redirect(url_for('views.home'))
 
 
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=username).first()
 
-    return redirect(url_for("views.home"))
+    if not user:
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.home'))
+
+    posts = Post.query.filter_by(author=user.id).all()
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
